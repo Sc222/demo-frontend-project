45ee3a29635d3436e974d63c5cc144f64b59de57
Sc222 <v.a.shaternikov@gmail.com>
2021-06-11 10:29:47 +0500

    add more demo files

---
+++ src/reactComponent.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./style.css";
+
+function ReactComponent() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Edit <code> page code </code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://altecsystems.ru/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Altec Systems
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default ReactComponent;
