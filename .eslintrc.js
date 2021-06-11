cec07f254a7963e62c9ce1fb017e1ee46fc93292
Sc222 <v.a.shaternikov@gmail.com>
2021-06-11 10:26:53 +0500

    update dependencies and add demo eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-    "extends": "react-app",
-    "rules" : {
-
-        // More custom rules here
-    }
-}
+    // extend some company style config can be used instead of react-app (airbnb for example)
+    extends: ["react-app", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+        "prettier/prettier": ["error"],
+    },
+};
